12f65c75801f67ce36f7669b60893159094236ff
mugi-mmder <toy2-dm2@mugiel.info>
2023-02-25 14:26:31 +0900

    jisho-test
    jisho-test

--- operator.py
+++ operator.py
@@ -1,5 +1,9 @@
 # Copyright (c) 2022 mugi
 import bpy
+import csv
+import re
+import os,os.path
+
 
 from bpy.types import Panel
 
@@ -29,24 +33,53 @@ class BONE_OT_mmdtools_bone_jpname_all_overwrite(bpy.types.Operator):
         print("=========  act_p_bones  =========")
         print(act_p_bones)
 
+
+        input_file = os.path.join(os.path.dirname(__file__), "bone-chikan.csv")#アドオンと同階層のCSVパス指定
+        # print("input_file----",input_file)
+        csvchikan = []
+        chklist = []
+
+        with open(input_file, mode = "r", encoding="shift_jis") as f:       #CSVファイルをリスト型で読み込む
+              reader = csv.reader(f)
+              print("reader---->\n",reader)
+              for row in reader:
+                    csvchikan.append(row)      #変換用の読み込み
+                    chklist.append(row[1])    #ざっくり確認用の読み込み
+        # print("(++++csvchikan+++++\n",csvchikan,"\n+++++++++++++++++++++")
+        # print("(++++chklist+++++\n",chklist,"\n+++++++++++++++++++++")
+
         for p_bone in act_p_bones:
             mmd_bone = p_bone.mmd_bone
 
             print("=========  p_bone  =========")
+            print("chklist--->",p_bone.name in chklist)
             print("len===",len(mmd_bone.name_j),"name======",p_bone.name)
             # print("_dummy_" in p_bone.name or "_shadow_" in p_bone.name)
             if not ("_dummy_" in p_bone.name or "_shadow_" in p_bone.name): #_dummy_&_shadow_は無視
 
-                if (p_bone.name.endswith(".L")):
-                    boneLRname = "左" + p_bone.name[0:-2]
-                    # print(p_bone.name.endswith(".L"),"==書き換え＝＝",p_bone.name,"--->",boneLRname)
 
-                elif(p_bone.name.endswith(".R")):
-                    boneLRname = "右" + p_bone.name[0:-2]
-                    # print(p_bone.name.endswith(".R"),"==書き換え＝＝",p_bone.name,"--->",boneLRname)
+                
+                if (p_bone.name in chklist):                       # 完全一致の英名はCSVリストから和名置換
+                    for chikan in csvchikan:
+                        # print(chikan)
+                        # print(chikan[1], chikan[0])
+                        if chikan[1] == p_bone.name:
+                           boneRename = chikan[0]
+
+                        # boneRename = p_bone.name.replace(csvchikan[1], csvchikan[0])
+                    print(p_bone.name in chklist,"==書き換え＝＝",p_bone.name,"--->",boneRename)
+
+                elif (p_bone.name.endswith((".L" , "-L" , "_L" ))): # 該当XXX*Lの書き方は左XXXに置換
+                    boneRename = "左" + p_bone.name[0:-2]
+                    # print(p_bone.name.endswith(".L"),"==書き換え＝＝",p_bone.name,"--->",boneRename)
+
+                elif (p_bone.name.endswith((".R" , "-R" , "_R" ))): # 該当XXX*Rの書き方は右XXXに置換
+                    boneRename = "右" + p_bone.name[0:-2]
+                    # print(p_bone.name.endswith(".R"),"==書き換え＝＝",p_bone.name,"--->",boneRename)
                 else:
-                    boneLRname = p_bone.name
-                mmd_bone.name_j = boneLRname
+                    boneRename = p_bone.name                        # 該当なしならボーン名ままなにもしない
+
+                mmd_bone.name_j = boneRename
                 print(" mmd_bone.name_j  --->",mmd_bone.name_j)
 
 
